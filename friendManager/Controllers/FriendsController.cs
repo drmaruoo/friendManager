460ea67aaaa4ebe9da9bb2fbf478d9a38c47ace2
Marcin Domarski <31709302+drmaruoo@users.noreply.github.com>
2018-06-10 21:51:13 +0200

    Removed some unused usings and obsolete comments

--- friendManager/Controllers/FriendsController.cs
+++ friendManager/Controllers/FriendsController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using friendManager.Models;
 
@@ -81,8 +79,6 @@ namespace friendManager.Controllers
         }
 
         // POST: Friends/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ID,Name,FirstMeetDate,BiggestSecret,Likeability")] Friend friend)
@@ -113,8 +109,6 @@ namespace friendManager.Controllers
         }
 
         // POST: Friends/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("ID,Name,FirstMeetDate,BiggestSecret,Likeability")] Friend friend)
--- friendManager/Models/ReguiredAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace friendManager.Models
-{
-    internal class ReguiredAttribute : Attribute
-    {
-    }
-}
